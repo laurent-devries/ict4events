8367ab52537e1757236bd6d361b7f283e365a7a0
laurent-devries <laurent-devries@outlook.com>
2015-04-10 16:30:57 +0200

    Nieuwsfeeds bitches

--- ICT4Events/Form1.Designer.cs
+++ ICT4Events/Form1.Designer.cs
@@ -37,18 +37,9 @@
             this.lblIngelogdNaam = new System.Windows.Forms.Label();
             this.pbProfilePicture = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pnlCategoryTopLeft = new System.Windows.Forms.Panel();
-            this.tbBeschrijving1 = new System.Windows.Forms.RichTextBox();
-            this.lblLikes = new System.Windows.Forms.Label();
-            this.lblViews1 = new System.Windows.Forms.Label();
-            this.lblDatum1 = new System.Windows.Forms.Label();
-            this.lblTitel1 = new System.Windows.Forms.Label();
-            this.pbMedia1 = new System.Windows.Forms.PictureBox();
             this.pnlNewsFeed = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pbProfilePicture)).BeginInit();
             this.panel1.SuspendLayout();
-            this.pnlCategoryTopLeft.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbMedia1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnHome
@@ -121,7 +112,7 @@
             this.lblIngelogdNaam.AutoSize = true;
             this.lblIngelogdNaam.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblIngelogdNaam.ForeColor = System.Drawing.Color.DarkOrange;
-            this.lblIngelogdNaam.Location = new System.Drawing.Point(843, 16);
+            this.lblIngelogdNaam.Location = new System.Drawing.Point(837, 18);
             this.lblIngelogdNaam.Name = "lblIngelogdNaam";
             this.lblIngelogdNaam.Size = new System.Drawing.Size(128, 16);
             this.lblIngelogdNaam.TabIndex = 6;
@@ -129,8 +120,8 @@
             // 
             // pbProfilePicture
             // 
-            this.pbProfilePicture.BackColor = System.Drawing.Color.Transparent;
-            this.pbProfilePicture.Location = new System.Drawing.Point(787, 4);
+            this.pbProfilePicture.BackColor = System.Drawing.Color.Maroon;
+            this.pbProfilePicture.Location = new System.Drawing.Point(781, 4);
             this.pbProfilePicture.Name = "pbProfilePicture";
             this.pbProfilePicture.Size = new System.Drawing.Size(50, 45);
             this.pbProfilePicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -148,82 +139,14 @@
             this.panel1.Controls.Add(this.btnCategorie);
             this.panel1.Controls.Add(this.tbSearch);
             this.panel1.Controls.Add(this.btnTags);
-            this.panel1.Location = new System.Drawing.Point(3, -4);
+            this.panel1.Location = new System.Drawing.Point(-1, -4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(974, 49);
+            this.panel1.Size = new System.Drawing.Size(978, 49);
             this.panel1.TabIndex = 8;
-            // 
-            // pnlCategoryTopLeft
-            // 
-            this.pnlCategoryTopLeft.BackColor = System.Drawing.Color.Gainsboro;
-            this.pnlCategoryTopLeft.Controls.Add(this.tbBeschrijving1);
-            this.pnlCategoryTopLeft.Controls.Add(this.lblLikes);
-            this.pnlCategoryTopLeft.Controls.Add(this.lblViews1);
-            this.pnlCategoryTopLeft.Controls.Add(this.lblDatum1);
-            this.pnlCategoryTopLeft.Controls.Add(this.lblTitel1);
-            this.pnlCategoryTopLeft.Controls.Add(this.pbMedia1);
-            this.pnlCategoryTopLeft.Location = new System.Drawing.Point(176, 51);
-            this.pnlCategoryTopLeft.Name = "pnlCategoryTopLeft";
-            this.pnlCategoryTopLeft.Size = new System.Drawing.Size(200, 216);
-            this.pnlCategoryTopLeft.TabIndex = 9;
-            // 
-            // tbBeschrijving1
-            // 
-            this.tbBeschrijving1.BackColor = System.Drawing.Color.Gainsboro;
-            this.tbBeschrijving1.Location = new System.Drawing.Point(18, 157);
-            this.tbBeschrijving1.Name = "tbBeschrijving1";
-            this.tbBeschrijving1.Size = new System.Drawing.Size(160, 56);
-            this.tbBeschrijving1.TabIndex = 5;
-            this.tbBeschrijving1.Text = "Dit is een beschrijving bitches";
-            // 
-            // lblLikes
-            // 
-            this.lblLikes.AutoSize = true;
-            this.lblLikes.Location = new System.Drawing.Point(82, 35);
-            this.lblLikes.Name = "lblLikes";
-            this.lblLikes.Size = new System.Drawing.Size(44, 13);
-            this.lblLikes.TabIndex = 4;
-            this.lblLikes.Text = "Likes: 1";
-            // 
-            // lblViews1
-            // 
-            this.lblViews1.AutoSize = true;
-            this.lblViews1.Location = new System.Drawing.Point(15, 35);
-            this.lblViews1.Name = "lblViews1";
-            this.lblViews1.Size = new System.Drawing.Size(47, 13);
-            this.lblViews1.TabIndex = 3;
-            this.lblViews1.Text = "Views: 1";
-            // 
-            // lblDatum1
-            // 
-            this.lblDatum1.AutoSize = true;
-            this.lblDatum1.Location = new System.Drawing.Point(15, 22);
-            this.lblDatum1.Name = "lblDatum1";
-            this.lblDatum1.Size = new System.Drawing.Size(38, 13);
-            this.lblDatum1.TabIndex = 2;
-            this.lblDatum1.Text = "Datum";
-            // 
-            // lblTitel1
-            // 
-            this.lblTitel1.AutoSize = true;
-            this.lblTitel1.Location = new System.Drawing.Point(15, 9);
-            this.lblTitel1.Name = "lblTitel1";
-            this.lblTitel1.Size = new System.Drawing.Size(27, 13);
-            this.lblTitel1.TabIndex = 1;
-            this.lblTitel1.Text = "Titel";
-            // 
-            // pbMedia1
-            // 
-            this.pbMedia1.Location = new System.Drawing.Point(18, 54);
-            this.pbMedia1.Name = "pbMedia1";
-            this.pbMedia1.Size = new System.Drawing.Size(160, 100);
-            this.pbMedia1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pbMedia1.TabIndex = 0;
-            this.pbMedia1.TabStop = false;
             // 
             // pnlNewsFeed
             // 
-            this.pnlNewsFeed.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pnlNewsFeed.BackColor = System.Drawing.SystemColors.InactiveBorder;
             this.pnlNewsFeed.Location = new System.Drawing.Point(137, 51);
             this.pnlNewsFeed.Name = "pnlNewsFeed";
             this.pnlNewsFeed.Size = new System.Drawing.Size(729, 395);
@@ -236,16 +159,14 @@
             this.BackColor = System.Drawing.SystemColors.Menu;
             this.ClientSize = new System.Drawing.Size(976, 513);
             this.Controls.Add(this.pnlNewsFeed);
-            this.Controls.Add(this.pnlCategoryTopLeft);
             this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(992, 552);
             this.Name = "FormBasicGUI";
             this.Text = "Basic user interface";
+            this.Resize += new System.EventHandler(this.FormBasicGUI_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.pbProfilePicture)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.pnlCategoryTopLeft.ResumeLayout(false);
-            this.pnlCategoryTopLeft.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbMedia1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -261,13 +182,6 @@
         private System.Windows.Forms.Label lblIngelogdNaam;
         private System.Windows.Forms.PictureBox pbProfilePicture;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel pnlCategoryTopLeft;
-        private System.Windows.Forms.PictureBox pbMedia1;
-        private System.Windows.Forms.Label lblDatum1;
-        private System.Windows.Forms.Label lblTitel1;
-        private System.Windows.Forms.Label lblLikes;
-        private System.Windows.Forms.Label lblViews1;
-        private System.Windows.Forms.RichTextBox tbBeschrijving1;
         private System.Windows.Forms.Panel pnlNewsFeed;
     }
 }
--- ICT4Events/Form1.cs
+++ ICT4Events/Form1.cs
@@ -10,22 +10,60 @@ using System.Windows.Forms;
 
 namespace ICT4Events
 {
+    
     public partial class FormBasicGUI : Form
     {
+        int count = 0;
+        List<NewsfeedItem> itemlist = new List<NewsfeedItem>();
         public FormBasicGUI()
         {
             InitializeComponent();
 
-            
-            TextBox titel = new TextBox();
+            lblIngelogdNaam.Location = new Point(this.Width - lblIngelogdNaam.Width - 30, 18);
+            pbProfilePicture.Location = new Point(this.Width - lblIngelogdNaam.Width - 85, 2);
+
+            panel1.Location = new Point(0, 0);
+            panel1.Width = Width;
+
+            pnlNewsFeed.Location = new Point(Width / 6, Height / 10);
+            pnlNewsFeed.Width = Width / 6 * 4;
+            pnlNewsFeed.Height = Height / 10 * 8;
+
             Panel p = new Panel();
-            p.Location = new Point(50, 50);
-            p.Height = 200;
-            p.BackColor = Color.Red;
-            p.Visible = true;
-            pnlNewsFeed.Controls.Add(p);
-            NewsfeedItem item = new NewsfeedItem("Titel", "datum", "Views", "Likes", "Message", p);
+            NewsfeedItem item1 = new NewsfeedItem("Titel", "datum", "Views", "Likes", "Message", p, pnlNewsFeed, count);
+            count += 1;
+            Panel p2 = new Panel();
+            NewsfeedItem item2 = new NewsfeedItem("Titel", "datum", "Views", "Likes", "Message", p2, pnlNewsFeed, count);
+            count += 1;
+            Panel p3 = new Panel();
+            NewsfeedItem item3 = new NewsfeedItem("Titel", "datum", "Views", "Likes", "Message", p3, pnlNewsFeed, count);
+
+            itemlist.Add(item1);
+            itemlist.Add(item2);
+            itemlist.Add(item3);
             
+
+            foreach (NewsfeedItem item in itemlist)
+            {
+                pnlNewsFeed.Controls.Add(item.Panel);                
+            }
+            
+        }
+
+        private void FormBasicGUI_Resize(object sender, EventArgs e)
+        {
+            pnlNewsFeed.Refresh();
+            panel1.Width = this.Width;
+            pnlNewsFeed.Width = this.Width / 6 * 4;
+            pnlNewsFeed.Height = this.Height / 10 * 8;
+
+            lblIngelogdNaam.Location = new Point(this.Width - lblIngelogdNaam.Width - 30, 18);
+            pbProfilePicture.Location = new Point(this.Width - lblIngelogdNaam.Width - 85, 2);
+
+            foreach(NewsfeedItem item in itemlist)
+            {
+                item.Panel.Location = new Point(pnlNewsFeed.Width / 3 * item.Count + 10, 10);
+            }
         }
 
         /*public void loadItems
