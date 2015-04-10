2279bd9e7c4d6d250d03bda17d3b00a6c492bdf4
laurent-devries <laurent-devries@outlook.com>
2015-04-10 10:16:34 +0200

    Added Class Comment

--- ICT4Events/Comment.cs
+++ ICT4Events/Comment.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace ICT4Events
 {
+    //Laurent de Vries
     class Comment
     {
+        //Fields
+        int id_Comment;
+        string comment;
+        DateTime Date { get; set; }
+        
+        //Constructor
+        public Comment(int id_Comment, DateTime date, string comment)
+        {
+            this.id_Comment = id_Comment;
+            this.comment = comment;
+            Date = date;
+        }
+
+        public bool checkAbuse()
+        {
+            return true;
+        }
     }
 }
