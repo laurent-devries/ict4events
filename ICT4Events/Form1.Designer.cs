fcafa307430fe0aa825067e628e230f22e6bbc5d
laurent-devries <laurent-devries@outlook.com>
2015-04-10 11:25:35 +0200

    Basic user interface design with 1 picturebox

--- ICT4Events/Form1.Designer.cs
+++ ICT4Events/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ICT4Events
 {
-    partial class Form1
+    partial class lblLikes2
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,239 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lblLikes2));
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnCategorie = new System.Windows.Forms.Button();
+            this.btnTags = new System.Windows.Forms.Button();
+            this.btnMateriaalHuren = new System.Windows.Forms.Button();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblIngelogdNaam = new System.Windows.Forms.Label();
+            this.pbProfilePicture = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlCategoryTopLeft = new System.Windows.Forms.Panel();
+            this.pbMedia1 = new System.Windows.Forms.PictureBox();
+            this.lblTitel1 = new System.Windows.Forms.Label();
+            this.lblDatum1 = new System.Windows.Forms.Label();
+            this.lblViews1 = new System.Windows.Forms.Label();
+            this.lblLikes = new System.Windows.Forms.Label();
+            this.tbBeschrijving1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProfilePicture)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.pnlCategoryTopLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMedia1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.Wheat;
+            this.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnHome.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.DarkOrange;
+            this.btnHome.Location = new System.Drawing.Point(0, 14);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(89, 23);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = false;
+            // 
+            // btnCategorie
+            // 
+            this.btnCategorie.BackColor = System.Drawing.Color.Wheat;
+            this.btnCategorie.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCategorie.ForeColor = System.Drawing.Color.DarkOrange;
+            this.btnCategorie.Location = new System.Drawing.Point(95, 14);
+            this.btnCategorie.Name = "btnCategorie";
+            this.btnCategorie.Size = new System.Drawing.Size(89, 23);
+            this.btnCategorie.TabIndex = 1;
+            this.btnCategorie.Text = "Categorie";
+            this.btnCategorie.UseVisualStyleBackColor = false;
+            // 
+            // btnTags
+            // 
+            this.btnTags.BackColor = System.Drawing.Color.Wheat;
+            this.btnTags.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTags.ForeColor = System.Drawing.Color.DarkOrange;
+            this.btnTags.Location = new System.Drawing.Point(190, 14);
+            this.btnTags.Name = "btnTags";
+            this.btnTags.Size = new System.Drawing.Size(89, 23);
+            this.btnTags.TabIndex = 2;
+            this.btnTags.Text = "Tags";
+            this.btnTags.UseVisualStyleBackColor = false;
+            // 
+            // btnMateriaalHuren
+            // 
+            this.btnMateriaalHuren.BackColor = System.Drawing.Color.Wheat;
+            this.btnMateriaalHuren.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMateriaalHuren.ForeColor = System.Drawing.Color.DarkOrange;
+            this.btnMateriaalHuren.Location = new System.Drawing.Point(285, 14);
+            this.btnMateriaalHuren.Name = "btnMateriaalHuren";
+            this.btnMateriaalHuren.Size = new System.Drawing.Size(134, 23);
+            this.btnMateriaalHuren.TabIndex = 3;
+            this.btnMateriaalHuren.Text = "Materiaal Huren";
+            this.btnMateriaalHuren.UseVisualStyleBackColor = false;
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(585, 16);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(100, 20);
+            this.tbSearch.TabIndex = 4;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(691, 16);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(30, 20);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "S";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // lblIngelogdNaam
+            // 
+            this.lblIngelogdNaam.AutoSize = true;
+            this.lblIngelogdNaam.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngelogdNaam.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lblIngelogdNaam.Location = new System.Drawing.Point(843, 16);
+            this.lblIngelogdNaam.Name = "lblIngelogdNaam";
+            this.lblIngelogdNaam.Size = new System.Drawing.Size(128, 16);
+            this.lblIngelogdNaam.TabIndex = 6;
+            this.lblIngelogdNaam.Text = "Laurent de Vries";
+            // 
+            // pbProfilePicture
+            // 
+            this.pbProfilePicture.BackColor = System.Drawing.Color.Transparent;
+            this.pbProfilePicture.Image = ((System.Drawing.Image)(resources.GetObject("pbProfilePicture.Image")));
+            this.pbProfilePicture.Location = new System.Drawing.Point(787, 4);
+            this.pbProfilePicture.Name = "pbProfilePicture";
+            this.pbProfilePicture.Size = new System.Drawing.Size(50, 45);
+            this.pbProfilePicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbProfilePicture.TabIndex = 7;
+            this.pbProfilePicture.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.pbProfilePicture);
+            this.panel1.Controls.Add(this.btnMateriaalHuren);
+            this.panel1.Controls.Add(this.lblIngelogdNaam);
+            this.panel1.Controls.Add(this.btnHome);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.btnCategorie);
+            this.panel1.Controls.Add(this.tbSearch);
+            this.panel1.Controls.Add(this.btnTags);
+            this.panel1.Location = new System.Drawing.Point(3, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(974, 49);
+            this.panel1.TabIndex = 8;
+            // 
+            // pnlCategoryTopLeft
+            // 
+            this.pnlCategoryTopLeft.BackColor = System.Drawing.Color.Gainsboro;
+            this.pnlCategoryTopLeft.Controls.Add(this.tbBeschrijving1);
+            this.pnlCategoryTopLeft.Controls.Add(this.lblLikes);
+            this.pnlCategoryTopLeft.Controls.Add(this.lblViews1);
+            this.pnlCategoryTopLeft.Controls.Add(this.lblDatum1);
+            this.pnlCategoryTopLeft.Controls.Add(this.lblTitel1);
+            this.pnlCategoryTopLeft.Controls.Add(this.pbMedia1);
+            this.pnlCategoryTopLeft.Location = new System.Drawing.Point(176, 51);
+            this.pnlCategoryTopLeft.Name = "pnlCategoryTopLeft";
+            this.pnlCategoryTopLeft.Size = new System.Drawing.Size(200, 216);
+            this.pnlCategoryTopLeft.TabIndex = 9;
+            // 
+            // pbMedia1
+            // 
+            this.pbMedia1.Image = ((System.Drawing.Image)(resources.GetObject("pbMedia1.Image")));
+            this.pbMedia1.Location = new System.Drawing.Point(18, 54);
+            this.pbMedia1.Name = "pbMedia1";
+            this.pbMedia1.Size = new System.Drawing.Size(160, 100);
+            this.pbMedia1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbMedia1.TabIndex = 0;
+            this.pbMedia1.TabStop = false;
+            // 
+            // lblTitel1
+            // 
+            this.lblTitel1.AutoSize = true;
+            this.lblTitel1.Location = new System.Drawing.Point(15, 9);
+            this.lblTitel1.Name = "lblTitel1";
+            this.lblTitel1.Size = new System.Drawing.Size(27, 13);
+            this.lblTitel1.TabIndex = 1;
+            this.lblTitel1.Text = "Titel";
+            // 
+            // lblDatum1
+            // 
+            this.lblDatum1.AutoSize = true;
+            this.lblDatum1.Location = new System.Drawing.Point(15, 22);
+            this.lblDatum1.Name = "lblDatum1";
+            this.lblDatum1.Size = new System.Drawing.Size(38, 13);
+            this.lblDatum1.TabIndex = 2;
+            this.lblDatum1.Text = "Datum";
+            // 
+            // lblViews1
+            // 
+            this.lblViews1.AutoSize = true;
+            this.lblViews1.Location = new System.Drawing.Point(15, 35);
+            this.lblViews1.Name = "lblViews1";
+            this.lblViews1.Size = new System.Drawing.Size(47, 13);
+            this.lblViews1.TabIndex = 3;
+            this.lblViews1.Text = "Views: 1";
+            // 
+            // lblLikes
+            // 
+            this.lblLikes.AutoSize = true;
+            this.lblLikes.Location = new System.Drawing.Point(82, 35);
+            this.lblLikes.Name = "lblLikes";
+            this.lblLikes.Size = new System.Drawing.Size(44, 13);
+            this.lblLikes.TabIndex = 4;
+            this.lblLikes.Text = "Likes: 1";
+            // 
+            // tbBeschrijving1
+            // 
+            this.tbBeschrijving1.BackColor = System.Drawing.Color.Gainsboro;
+            this.tbBeschrijving1.Location = new System.Drawing.Point(18, 157);
+            this.tbBeschrijving1.Name = "tbBeschrijving1";
+            this.tbBeschrijving1.Size = new System.Drawing.Size(160, 56);
+            this.tbBeschrijving1.TabIndex = 5;
+            this.tbBeschrijving1.Text = "Dit is een beschrijving bitches";
+            // 
+            // lblLikes2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(976, 513);
+            this.Controls.Add(this.pnlCategoryTopLeft);
+            this.Controls.Add(this.panel1);
+            this.Name = "lblLikes2";
+            this.Text = "Basic user interface";
+            ((System.ComponentModel.ISupportInitialize)(this.pbProfilePicture)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.pnlCategoryTopLeft.ResumeLayout(false);
+            this.pnlCategoryTopLeft.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMedia1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnCategorie;
+        private System.Windows.Forms.Button btnTags;
+        private System.Windows.Forms.Button btnMateriaalHuren;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblIngelogdNaam;
+        private System.Windows.Forms.PictureBox pbProfilePicture;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlCategoryTopLeft;
+        private System.Windows.Forms.PictureBox pbMedia1;
+        private System.Windows.Forms.Label lblDatum1;
+        private System.Windows.Forms.Label lblTitel1;
+        private System.Windows.Forms.Label lblLikes;
+        private System.Windows.Forms.Label lblViews1;
+        private System.Windows.Forms.RichTextBox tbBeschrijving1;
     }
 }
 
--- ICT4Events/Program.cs
+++ ICT4Events/Program.cs
@@ -16,7 +16,7 @@ namespace ICT4Events
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new lblLikes2());
         }
     }
 }
