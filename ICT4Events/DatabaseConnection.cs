481f4b669d36301ef06820824efbf5db7a45b043
frankhartman <frankhartman96@gmail.com>
2015-04-14 13:52:24 +0200

    DatabaseConnection has been added

---
+++ ICT4Events/DatabaseConnection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+namespace test
+{
+    public class DatabaseConnection
+    {
+        private OracleConnection oracleConn;
+
+        private string user = "system";//Wachtwoord van de server
+        private string pw = "visisdood";//Wachtwoord van de server
+
+        private void Connect()
+        {
+            try
+            {
+                oracleConn = new OracleConnection("User Id=" + user + ";Password=" + pw + ";Data Source=" + "//localhost:1521/XE" + ";");
+                MessageBox.Show("Connectie is gelukt");
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        
+        public List<string> SelectFromDatabase(string Querry)
+        {
+            List<string> dataList = new List<string>();
+            try
+            {
+                Connect();
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = oracleConn;
+                cmd.CommandText = Querry;
+                cmd.CommandType = CommandType.Text;
+                oracleConn.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+
+                MessageBox.Show(reader.RowSize.ToString());
+                while (reader.Read())
+                {
+                    dataList.Add(reader.GetString(0));
+                }
+                return dataList;
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return null;
+            }
+
+        }
+    }
+}
