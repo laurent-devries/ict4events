80bd0b6c03a4c27f79497701a669aabf4d927cc8
laurent-devries <laurent-devries@outlook.com>
2015-04-15 18:05:57 +0200

    AddUploadingPage

--- ICT4Events/SocialSharing.cs
+++ ICT4Events/SocialSharing.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace ICT4Events
 {
+        
     public partial class SocialSharing : Form
     {
         int countWidth = 0;
@@ -22,6 +23,10 @@ namespace ICT4Events
 
         List<NewsFeedItem> itemlist = new List<NewsFeedItem>();
         List<Media> mediaList;
+
+        TextBox tTitleOfMedia;
+        RichTextBox tMediaDescription;
+        TextBox tMediaPath;
 
 
         public SocialSharing()
@@ -148,8 +153,90 @@ namespace ICT4Events
         private void button2_Click(object sender, EventArgs e)
         {
             pnlNewsFeed.Controls.Clear();
+            btnNextPage.Visible = false;
 
+            loadUploadingScreen();
+        }
 
+        public void loadUploadingScreen()
+        {
+            //Titel
+            Label Titel = new Label();
+            Titel.Location = new Point(pnlNewsFeed.Width / 2, 5);
+            Titel.Text = "Upload";
+            Titel.ForeColor = Color.DarkOrange;
+            Titel.Font = new Font("Georgia", 20, FontStyle.Underline);
+            Titel.Width = pnlNewsFeed.Width - 10;
+            Titel.Height = 40;
+            pnlNewsFeed.Controls.Add(Titel);
+
+            
+
+            //TitelOfMedia
+            Label lTitleOfMedia = new Label();
+            lTitleOfMedia.Location = new Point(10 , pnlNewsFeed.Height / 10 * 2);
+            lTitleOfMedia.Text = "Title of Media";
+            lTitleOfMedia.ForeColor = Color.DarkOrange;
+            lTitleOfMedia.Font = new Font("Georgia", 13);
+            lTitleOfMedia.Width = 140;
+            lTitleOfMedia.Height = 20;
+            pnlNewsFeed.Controls.Add(lTitleOfMedia);
+
+            tTitleOfMedia = new TextBox();
+            tTitleOfMedia.Location = new Point(lTitleOfMedia.Width + 10, pnlNewsFeed.Height / 10 * 2);
+            tTitleOfMedia.Width = 150;
+            pnlNewsFeed.Controls.Add(tTitleOfMedia);
+            
+            //MediaDescription
+            Label lMediaDescription = new Label();
+            lMediaDescription.Location = new Point(10, pnlNewsFeed.Height / 10 * 3);
+            lMediaDescription.Text = "Description";
+            lMediaDescription.ForeColor = Color.DarkOrange;
+            lMediaDescription.Font = new Font("Georgia", 13);
+            lMediaDescription.Width = 140;
+            lMediaDescription.Height = 20;
+            pnlNewsFeed.Controls.Add(lMediaDescription);
+
+            tMediaDescription = new RichTextBox();
+            tMediaDescription.Location = new Point(lTitleOfMedia.Width + 10, pnlNewsFeed.Height / 10 * 3);
+            tMediaDescription.Width = 150;
+            tMediaDescription.Height = tTitleOfMedia.Height * 3 ;
+            pnlNewsFeed.Controls.Add(tMediaDescription);
+
+            //MediaPath
+            Label lMediaPath = new Label();
+            lMediaPath.Location = new Point(10, pnlNewsFeed.Height / 10 * 5);
+            lMediaPath.Text = "MediaPath";
+            lMediaPath.ForeColor = Color.DarkOrange;
+            lMediaPath.Font = new Font("Georgia", 13);
+            lMediaPath.Width = 140;
+            lMediaPath.Height = 20;
+            pnlNewsFeed.Controls.Add(lMediaPath);
+
+            Button bMediaPath = new Button();
+            bMediaPath.Location = new Point(lTitleOfMedia.Width + 10, pnlNewsFeed.Height / 10 * 5 - 1);
+            bMediaPath.Text = "Browse";
+            pnlNewsFeed.Controls.Add(bMediaPath);
+
+            Button bUpload = new Button();
+            bUpload.Location = new Point(lTitleOfMedia.Width + 10, pnlNewsFeed.Height / 10 * 5 + bMediaPath.Height );
+            bUpload.Text = "Upload";
+            pnlNewsFeed.Controls.Add(bUpload);
+
+            tMediaPath = new TextBox();
+            tMediaPath.Location = new Point(lTitleOfMedia.Width + bMediaPath.Width + 10, pnlNewsFeed.Height / 10 * 5);
+            tMediaPath.Width = 150 - bMediaPath.Width;
+            pnlNewsFeed.Controls.Add(tMediaPath);
+
+            //Preview
+            Label lPreview = new Label();
+            lPreview.Location = new Point(pnlNewsFeed.Width / 4 * 3 - 20, pnlNewsFeed.Height / 10 * 2);
+            lPreview.Text = "Preview";
+            lPreview.ForeColor = Color.DarkOrange;
+            lPreview.Font = new Font("Georgia", 13);
+            lPreview.Width = 140;
+            lPreview.Height = 20;
+            pnlNewsFeed.Controls.Add(lPreview);
         }
 
        }
