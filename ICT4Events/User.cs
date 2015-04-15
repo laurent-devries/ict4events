cb1f07c594d4001e95d1211e15ef22c10a1ca579
frankhartman <frankhartman96@gmail.com>
2015-04-15 15:06:19 +0200

    added searchByRfid method

--- ICT4Events/User.cs
+++ ICT4Events/User.cs
@@ -112,5 +112,31 @@ namespace ICT4Events
             }
             
         }
+
+        public User SearchByRfid(string rfid)
+        {
+            try
+            {
+                DatabaseConnection con = new DatabaseConnection();
+                string Querry = "SELECT ID_USER, RFIDTAG, FIRSTNAME, SURNAME, BIRTHDATE, EMAIL, CITY, CELLPHONENUMBER, LOGINNAME, USERNAME, PASSWORDUSER, PROFILEPIC, SUMMARYUSER, PRESENTUSER FROM ICT4_USER WHERE rfid = " + rfid;
+
+                OracleDataReader reader = con.SelectFromDatabase(Querry);
+                User user;
+                while (reader.Read())
+                {
+                    user = new User(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetDateTime(4), reader.GetString(5), reader.GetString(6), reader.GetString(7), reader.GetString(8), reader.GetString(9), reader.GetString(10), reader.GetString(11), reader.GetString(12), Convert.ToChar(reader.GetString(13)));
+                    return user;
+                }
+                MessageBox.Show("Username and password combination does not excist");
+                return null;
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return null;
+            }
+
+        }
     }
 }
