2dd85eb4fd05b089b00a4f288bbac48c6dcc3f27
frankhartman <frankhartman96@gmail.com>
2015-04-17 11:52:09 +0200

    loginform and permissions updated

--- ICT4Events/Permission.cs
+++ ICT4Events/Permission.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Windows.Forms;
 
 namespace ICT4Events
 {
@@ -29,6 +32,36 @@ namespace ICT4Events
         {
             this.name = name;
             this.permission_value = permission_value;
+        }
+
+        public Permission()
+        {
+
+        }
+
+        public Permission GetPermissionUser(User u)
+        {
+            try
+            {
+                DatabaseConnection con = new DatabaseConnection();
+                string Querry = "SELECT PERMISSIONNAME, PERMISSIONVALUE FROM ICT4_PERMISSION WHERE ID_PERMISSION = '" + u.Permissionfk.ToString() + "'";
+
+                OracleDataReader reader = con.SelectFromDatabase(Querry);
+                Permission permission;
+                while (reader.Read())
+                {
+                    permission = new Permission(reader.GetString(0), reader.GetInt32(1));
+                    return permission;
+                }
+                MessageBox.Show("Permission not in system");
+                return null;
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return null;
+            }
         }
     }
 }
