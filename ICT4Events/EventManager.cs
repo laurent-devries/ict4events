792bf8a31089c52f2b41642b47c3685ad6abe0af
lyoeril <yoerivi@zeelandnet.nl>
2015-04-17 15:35:12 +0200

    Commit

---
+++ ICT4Events/EventManager.cs
@@ -0,0 +1,30 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Events
+{
+    class EventManager
+    {
+        private List<Event> evenementen;
+        public EventManager()
+        {
+            evenementen = new List<Event>();
+        }
+        public List<Event> RequestEvent()
+        {
+            DatabaseConnection con = new DatabaseConnection();
+            string Querry = "SELECT ID_EVENT, TITLE, DATEICT, STARTDATE, ENDDATE, CAMPINGNAME, LOCATION FROM ICT4_EVENT";
+            OracleDataReader reader = con.SelectFromDatabase(Querry);
+            while (reader.Read())
+            {
+                Event event1 = new Event(reader.GetString(1), reader.GetDateTime(2), reader.GetDateTime(4), reader.GetString(5), reader.GetString(6));
+                evenementen.Add(event1);
+            }
+            return evenementen;
+        }
+    }
+}
