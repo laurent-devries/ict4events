5f8bedd46b4c18d6a0cdb08c6f044fb2cc3d6e11
lyoeril <yoerivi@zeelandnet.nl>
2015-04-17 11:15:39 +0200

    user changes

--- ICT4Events/User.cs
+++ ICT4Events/User.cs
@@ -49,18 +49,26 @@ namespace ICT4Events
         public string Sur_Name { get { return sur_name; } set { sur_name = value; } }
         public string RFID_Tag { get { return rfid_tag; } set { rfid_tag = value; } }
         public int ID_User { get { return id_user; } }
+        public int ID_EventFK { get; set; }
+        public int ID_ReservationFK { get; set; }
+        public string Street { get; set; }
+        public string Housenumber { get; set; }
 
         public int Permissionfk { get { return permissionFk; } set { permissionFk = value; } }
 
-        public User(int id_user, int permission, string rfid_tag, string first_name, string sur_name, DateTime birth_date, string email, string city, string phone_number, string loginname, string username, string password, string profile_pic, string summary, char present, string country)
+        public User(int id_user, int id_event, int id_reservation, int permission, string first_name, string sur_name, DateTime birth_date, string email, string country, string street, string housenumber, string city, string phone_number, string loginname, string username, string password, string profile_pic, string summary, char present, string rfid_tag)
         {
             this.id_user = id_user;
+            ID_EventFK = id_event;
+            ID_ReservationFK = id_reservation;
             this.permissionFk = permission;
-            this.rfid_tag = rfid_tag;
             this.first_name = first_name;
             this.sur_name = sur_name;
             this.birth_date = birth_date;
             this.email = email;
+            this.country = country;
+            Street = street;
+            Housenumber = housenumber;
             this.city = city;
             this.phone_number = phone_number;
             this.loginname = loginname;
@@ -69,7 +77,31 @@ namespace ICT4Events
             this.profile_pic = profile_pic;
             this.summary = summary;
             this.present = present;
+            this.present = present;
+            this.rfid_tag = rfid_tag;
+        }
+        public User(int id_user, int id_event, int id_reservation, int permission, string first_name, string sur_name, DateTime birth_date, string email, string country, string street, string housenumber, string city, string phone_number, string loginname, string username, string password, string profile_pic, string summary, char present)
+        {
+            this.id_user = id_user;
+            ID_EventFK = id_event;
+            ID_ReservationFK = id_reservation;
+            this.permissionFk = permission;
+            this.first_name = first_name;
+            this.sur_name = sur_name;
+            this.birth_date = birth_date;
+            this.email = email;
             this.country = country;
+            Street = street;
+            Housenumber = housenumber;
+            this.city = city;
+            this.phone_number = phone_number;
+            this.loginname = loginname;
+            this.username = username;
+            this.password = password;
+            this.profile_pic = profile_pic;
+            this.summary = summary;
+            this.present = present;
+            this.present = present;
 
         }
         public User()
